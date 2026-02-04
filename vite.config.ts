ad0d64c84aa071b800205543bd14cd031a1cf718
Rasel Shikdar <imrasel597@gmail.com>
2026-02-04 20:07:43 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // Add this line to force absolute paths for Firebase
   server: {
     host: "::",
     port: 8080,
